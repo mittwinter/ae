dbaf814bf29551db92c5de8c0d703bfe15673fa7
Martin Wegner <mw@mroot.net>
2011-04-28 16:41:38 +0200

    Task 1.P1: Add implementation of vector_const.
    Allows contruction, push_back(), pop_back(), set(...), get() in
    absolutely constant time. Nothing amortized here.

---
+++ ex-1-p1/const_vector.hpp
@@ -0,0 +1,59 @@
+/* Author: Martin Wegner
+ * Released under the terms of LGPL v3 (http://www.gnu.org/licenses/lgpl-3.0.txt).
+ */
+#pragma once
+#ifndef CONST_VECTOR_HPP
+#define CONST_VECTOR_HPP
+
+#include <cassert>
+#include <stdint.h>
+
+template< typename T > class vector_const {
+	public:
+		vector_const() : capacity(1), used(0), toCopy(0), container(new T[1]), nextContainer(new T[2 * 1]) {}
+		virtual ~vector_const() {
+			delete[] container;
+			delete[] nextContainer;
+		}
+
+		void push_back(T const &e) {
+			assert(toCopy <= used);
+			assert(used < capacity);
+			// Incrementally copy elements from container to nextContainer:
+			nextContainer[toCopy] = container[toCopy];
+			++toCopy;
+			// Actually push back new element to both containers:
+			container[used] = e;
+			nextContainer[used] = e;
+			++used;
+			if(used == capacity) {
+				delete[] container;
+				container = nextContainer;
+				capacity *= 2;
+				nextContainer = new T[2 * capacity];
+				toCopy = 0;
+			}
+		}
+
+		void pop_back() { --used; }
+
+		T const &get(uint64_t p) const {
+			assert(p <= used);
+			return container[p];
+		}
+		void set(uint64_t p, T const &e) {
+			assert(p < size);
+			container[p] = e;
+		}
+		uint64_t size() const { return capacity; }
+
+	protected:
+		uint64_t capacity;
+		uint64_t used;
+		uint64_t toCopy;
+		T *container;
+		T *nextContainer;
+};
+
+#endif
+
